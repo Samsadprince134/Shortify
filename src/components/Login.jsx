18dcaf27b7e21cbe301b04a0e585b0c7dd62b818
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-08-02 05:26:53 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from 'react'
-import * as Yup from "yup"
+import * as Yup from 'yup'
 import useFetch from '@/hooks/use-fetch'
 import { login } from '@/db/apiAuth'
 import { useEffect } from 'react'
