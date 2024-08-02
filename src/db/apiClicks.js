b0167b00b8e8cf4ba52056d9f7f3c0c32c25df2c
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-08-02 19:29:24 +0530

    Update apiClicks.js

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -44,9 +44,12 @@ const parser = new UAParser();
 
 export const storeClicks = async ({id, originalUrl}) => {
   try {
-    const res = parser.getResult();
-    const device = res?.device?.type || "Mobile"; // Default to desktop if type is not detected
-
+   // const res = parser.getResult();
+  //  const device = res?.device?.type || "Mobile"; // Default to desktop if type is not detected
+    const res = parser.getOS();
+   // console.log("res" , res);
+    const device = res.name == "Windows" ? "desktop" : "mobile";
+      -------------------
     const response = await fetch("https://ipapi.co/json");
     const {city, country_name: country} = await response.json();
 
