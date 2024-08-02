96da32827576bc947af218bec0f0062c3be6815e
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-08-02 17:57:43 +0530

    Update LinkPage.jsx

--- src/pages/LinkPage.jsx
+++ src/pages/LinkPage.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://shortify-zeta.vercel.app/${link}`}
+            href={`https://shortify-xi-wheat.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://shortify-zeta.vercel.app/{link}
+            https://shortify-xi-wheat.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://shortify-zeta.vercel.app/${link}`)
+                navigator.clipboard.writeText(`https://shortify-xi-wheat.vercel.app/${link}`)
               }
             >
               <Copy />
